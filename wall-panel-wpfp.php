4b82c261ec3155e63506221a317005c985c094c2
Pablo Lires <pablolires@gmail.com>
2023-10-11 13:58:55 -0300

    agregado de dos productos del commit anterior. modificacion de modulo comun de productos

---
+++ wall-panel-wpfp.php
@@ -0,0 +1,175 @@
+<!-- Validaciones Newsletter -->
+<?php include_once('includes/validaciones-newsletter.inc'); ?>
+
+<!doctype html>
+<html lang="es">
+
+<head>
+  <!-- Tag Manager Head -->
+  <?php include_once("includes/tag_manager_head.inc"); ?>
+
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="wall pannel en MDF Fabricados en MDF prepintado blanco. Cada tira mide 110mm de ancho x 2750mm de alto y 9mm de espesor.
+Cada empaque contiene 10 tiras de revestimiento que cubren 2,90 m2.">
+
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" href="img/apple-icon.png">
+  <link rel="icon" href="img/favicon.png">
+
+  <!-- Normalize CSS -->
+  <link rel="stylesheet" type="text/css" href="css/normalize.min.css">
+
+  <!-- Animate CSS -->
+  <link rel="stylesheet" type="text/css" href="css/animate.css">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500,700" rel="stylesheet">
+
+  <!-- Fontawesome -->
+  <link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css">
+
+  <!-- Plugins magnific popup -->
+  <link href="plugins/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" type="text/css" href="css/app.css">
+
+  <title>Wall pannel en MDF Fabricados en MDF prepintado blanco.</title>
+</head>
+
+<body>
+  <!-- Tag Manager Body -->
+  <?php include_once("includes/tag_manager_body.inc"); ?>
+
+  <?php $current = 'productos'; ?>
+
+  <!-- WhatsApp -->
+  <?php include_once("includes/wapp.inc"); ?>
+
+  <!-- Header -->
+  <?php include_once('includes/header.inc'); ?>
+
+  <!-- Descripcion Producto -->
+  <section class="container mb-5 mt-5">
+
+    <div class="row">
+
+      <div class="col-lg-6 mb-5 parent-container">
+
+        <a class="transition productos_mini popup-img"
+          href="img/productos/wall-panel-wpfp/wall-panel-wpfp-principal.jpg" title="WALL PANEL - WPFP">
+          <img id="principal" class="wow fadeInUp img-fluid"
+            src="img/productos/wall-panel-wpfp/wall-panel-wpfp-principal.jpg" alt="WALL PANEL - Principal">
+        </a>
+
+        <div class="row">
+
+          <div class="col-4 offset-2">
+            <a class="transition productos_mini popup-img" title="WALL PANEL - WPFP"
+              href="img/productos/wall-panel-wpfp/wall-panel-wpfp-perfil.jpg">
+              <img id="perfil" class="wow fadeInRight img-fluid"
+                src="img/productos/wall-panel-wpfp/wall-panel-wpfp-perfil.jpg" alt="WALL PANEL - perfil">
+            </a>
+          </div>
+
+          <div class="col-4">
+            <a class="transition productos_mini popup-img" title="WALL PANEL - WPFP"
+              href="img/productos/wall-panel-wpfp/wall-panel-wpfp-render.jpg">
+              <img id="render" class="wow fadeInRight img-fluid"
+                src="img/productos/wall-panel-wpfp/wall-panel-wpfp-render.jpg" alt="WALL PANEL - render">
+            </a>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="productos col-lg-5 offset-lg-1 mb-5">
+        <h1 class="wow fadeInUp">WALL PANEL MDF</h1>
+        <h2 class="wow fadeInLeft">WPFP</h2>
+        <p class="wow fadeInUp">
+          <span class="font-weight-bold">Descripción:</span> <br>
+          Fabricados en MDF <br>
+          prepintado blanco.
+        </p>
+
+        <p class="wow fadeInUp">
+          <span class="font-weight-bold">Medidas:</span> <br>
+          Cada tira mide 110mm de ancho x 2750mm de alto
+        </p>
+
+        <p class="wow fadeInUp">
+          <span class="font-weight-bold">Espesor:</span> <br>
+          9 mm
+        </p>
+
+        <p class="wow fadeInUp">
+          <span class="font-weight-bold">Presentación:</span> <br>
+          Cada empaque contiene 10 tiras de revestimiento que cubren 2,90 m2.
+        </p>
+
+        <p class="wow fadeInUp">
+          <span class="font-weight-bold">Colores:</span> <br>
+          Prepintado
+        </p>
+
+        <div class="row">
+
+          <div class="wow fadeInUp content_colors col-md-12">
+            <div class="colors bg-blank">
+              &nbsp;
+            </div>
+          </div>
+
+        </div>
+
+        <div class="row">
+          <div class="col-md-12 text-center mb-3">
+            <h5 class="wow fadeInUp mb-3">Consultá por colores personalizados.</h5>
+            <a href="contacto.php" class="btn btn_site font_1em wow fadeInLeft">CONTACTO</a>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+  <!-- Descripcion Producto end -->
+
+  <!-- Otros Productos -->
+  <?php include_once('includes/otros-productos.inc'); ?>
+
+  <!-- Newsletter -->
+  <?php include_once('includes/newsletter.inc'); ?>
+
+  <!-- Mas Información -->
+  <?php include_once('includes/mas-informacion.inc'); ?>
+
+  <!-- Footer -->
+  <?php include_once('includes/footer.inc'); ?>
+
+  <!-- jQuery primero, luego Popper.js, luego Bootstrap JS -->
+  <script src="js/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <!-- Wow efecto para que aparezcan los objetos cuando se va scroleando -->
+  <script type="text/javascript" src="js/wow.min.js"></script>
+
+  <!-- Magnific Popup javascript -->
+  <script type="text/javascript" src="plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Pop Up Productos -->
+  <script type="text/javascript" src="js/pop-up-productos.js"></script>
+
+  <!-- Scripts -->
+  <script type="text/javascript" src="js/app.js"></script>
+
+</body>
+
+</html>
