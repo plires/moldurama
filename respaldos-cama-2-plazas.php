2f5aa3cac22b8e7fab41b0b3b3db8bc5602cf4b1
Pablo Lires <pablolires@gmail.com>
2024-07-15 14:59:09 -0300

    agregado de pages de respaldos de cama

---
+++ respaldos-cama-2-plazas.php
@@ -0,0 +1,195 @@
+<!-- Validaciones Newsletter -->
+<?php include_once('includes/validaciones-newsletter.inc'); ?>
+
+<!doctype html>
+<html lang="es">
+
+<head>
+  <!-- Tag Manager Head -->
+  <?php include_once("includes/tag_manager_head.inc"); ?>
+
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Respaldo de cama en MDF. Fabricados en MDF con dos colgadores para el montaje, lo que permite ajustar la altura según preferencia. Este respaldo mide 169 cm. de ancho y 91 cm. de alto con un espesor de 3cm. ">
+
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" href="img/apple-icon.png">
+  <link rel="icon" href="img/favicon.png">
+
+  <!-- Normalize CSS -->
+  <link rel="stylesheet" type="text/css" href="css/normalize.min.css">
+
+  <!-- Animate CSS -->
+  <link rel="stylesheet" type="text/css" href="css/animate.css">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500,700" rel="stylesheet">
+
+  <!-- Fontawesome -->
+  <link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css">
+
+  <!-- Plugins magnific popup -->
+  <link href="plugins/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" type="text/css" href="css/app.css">
+
+  <title>Respaldo de cama 2 plaza en MDF. Prepintado o color natural</title>
+</head>
+
+<body>
+  <!-- Tag Manager Body -->
+  <?php include_once("includes/tag_manager_body.inc"); ?>
+
+  <?php $current = 'productos'; ?>
+
+  <!-- WhatsApp -->
+  <?php include_once("includes/wapp.inc"); ?>
+
+  <!-- Header -->
+  <?php include_once('includes/header.inc'); ?>
+
+  <!-- Descripcion Producto -->
+  <section class="container product_description mb-5 mt-5">
+
+    <div class="row">
+
+      <div class="col-lg-6 mb-5 parent-container">
+
+        <a class="transition productos_mini popup-img" href="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-principal.jpg" title="RESPALDO DE CAMA 2 PLAZAS - MDF">
+          <img id="principal" class="wow fadeInUp img-fluid" src="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-principal.jpg" alt="RESPALDO DE CAMA 2 PLAZAS - MDF Principal">
+        </a>
+
+        <div class="row">
+
+          <div class="col-4 mb-4">
+            <a class="transition productos_mini popup-img" title="RESPALDO DE CAMA 2 PLAZAS - MDF" href="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-1.gif">
+              <img class="wow fadeInRight img-fluid" src="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-1.gif" alt="RESPALDO DE CAMA 2 PLAZAS - MDF 1">
+            </a>
+          </div>
+
+          <div class="col-4 mb-4">
+            <a class="transition productos_mini popup-img" title="RESPALDO DE CAMA 2 PLAZAS - MDF" href="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-2.jpg">
+              <img class="wow fadeInRight img-fluid" src="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-2.jpg" alt="RESPALDO DE CAMA 2 PLAZAS - MDF 2">
+            </a>
+          </div>
+
+          <div class="col-4 mb-4">
+            <a class="transition productos_mini popup-img" title="RESPALDO DE CAMA 2 PLAZAS - MDF" href="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-3.jpg">
+              <img class="wow fadeInRight img-fluid" src="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-3.jpg" alt="RESPALDO DE CAMA 2 PLAZAS - MDF 3">
+            </a>
+          </div>
+
+          <div class="col-4 mb-4">
+            <a class="transition productos_mini popup-img" title="RESPALDO DE CAMA 2 PLAZAS - MDF" href="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-4.jpg">
+              <img class="wow fadeInRight img-fluid" src="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-4.jpg" alt="RESPALDO DE CAMA 2 PLAZAS - MDF 4">
+            </a>
+          </div>
+
+          <div class="col-4 mb-4">
+            <a class="transition productos_mini popup-img" title="RESPALDO DE CAMA 2 PLAZAS - MDF" href="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-5.jpg">
+              <img class="wow fadeInRight img-fluid" src="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-5.jpg" alt="RESPALDO DE CAMA 2 PLAZAS - MDF 5">
+            </a>
+          </div>
+
+          <div class="col-4 mb-4">
+            <a class="transition productos_mini popup-img" title="RESPALDO DE CAMA 2 PLAZAS - MDF" href="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-6.jpg">
+              <img class="wow fadeInRight img-fluid" src="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-6.jpg" alt="RESPALDO DE CAMA 2 PLAZAS - MDF 6">
+            </a>
+          </div>
+
+          <div class="col-4">
+            <a class="transition productos_mini popup-img" title="RESPALDO DE CAMA 2 PLAZAS - MDF" href="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-7.jpg">
+              <img class="wow fadeInRight img-fluid" src="img/productos/respaldo-cama-2plaza/respaldo-cama-2plaza-7.jpg" alt="RESPALDO DE CAMA 2 PLAZAS - MDF 7">
+            </a>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="productos col-lg-5 offset-lg-1 mb-5">
+        <h1 class="wow fadeInUp">RESPALDO DE CAMA 2 PLAZAS</h1>
+        <h2 class="wow fadeInLeft">RESP 2P</h2>
+        <p class="wow fadeInUp">
+          <span class="font-weight-bold">Descripción:</span> <br>
+          Fabricados en MDF con dos colgadores para el montaje, lo que permite ajustar la altura según preferencia.
+        </p>
+
+        <p class="wow fadeInUp">
+          <span class="font-weight-bold">Medidas:</span> <br>
+          Este respaldo mide 169 cm. de ancho y 91 cm. de alto.
+        </p>
+
+        <p class="wow fadeInUp">
+          <span class="font-weight-bold">Espesor:</span> <br>
+          3 cm.
+        </p>
+
+        <p class="wow fadeInUp">
+          <span class="font-weight-bold">Colores:</span> <br>
+          Prepintado y Natural
+        </p>
+
+        <div class="row">
+
+          <div class="wow fadeInUp content_colors col-md-12">
+            <div class="colors bg-blank">
+              &nbsp;
+            </div>
+            <div class="colors bg-natural">
+              &nbsp;
+            </div>
+          </div>
+
+        </div>
+
+        <div class="row">
+          <div class="col-md-12 text-center mb-3">
+            <h5 class="wow fadeInUp mb-3">Consultá por colores personalizados.</h5>
+            <a href="contacto.php" class="btn btn_site font_1em wow fadeInLeft">CONTACTO</a>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+  <!-- Descripcion Producto end -->
+
+  <!-- Otros Productos -->
+  <?php include_once('includes/otros-productos.inc'); ?>
+
+  <!-- Newsletter -->
+  <?php include_once('includes/newsletter.inc'); ?>
+
+  <!-- Mas Información -->
+  <?php include_once('includes/mas-informacion.inc'); ?>
+
+  <!-- Footer -->
+  <?php include_once('includes/footer.inc'); ?>
+
+  <!-- jQuery primero, luego Popper.js, luego Bootstrap JS -->
+  <script src="js/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <!-- Wow efecto para que aparezcan los objetos cuando se va scroleando -->
+  <script type="text/javascript" src="js/wow.min.js"></script>
+
+  <!-- Magnific Popup javascript -->
+  <script type="text/javascript" src="plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Pop Up Productos -->
+  <script type="text/javascript" src="js/pop-up-productos.js"></script>
+
+  <!-- Scripts -->
+  <script type="text/javascript" src="js/app.js"></script>
+
+</body>
+
+</html>
